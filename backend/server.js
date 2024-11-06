baa746fbf36d874d3fe49fee737d309095dc09b4
Anton Cherepov <anton.cherepov@rabobank.nl>
2024-11-06 22:10:16 +0100

    add cors to be

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
-app.use(express.json());
+app.use(express.json(), cors());
 
 app.post('/register', (req, res) => {
   const { username, password, email, fullname } = req.body;
